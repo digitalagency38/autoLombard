507809b0ef18aee8a8ca1a87d252652bd424170b
Sergey Goncharovр <doimpos@gmail.com>
2023-03-03 22:40:01 +0500

    Доработки главной

--- src/blocks/modules/block_prev/block_prev.js
+++ src/blocks/modules/block_prev/block_prev.js
@@ -1,13 +1,13 @@
 import Glide, { Controls, Breakpoints } from '@glidejs/glide';
 
-const BlockSteps = class BlockSteps {
+const BlockPrev = class BlockPrev {
     constructor({sliderOptions}){
         this.sliderOptions = sliderOptions;
-        this.slider = new Glide('.step-glide-js', this.sliderOptions);
+        this.slider = new Glide('.prev-glide-js', this.sliderOptions);
     }
     initialSlider() {
         setTimeout(() => {
-            if (!document.querySelector('.step-glide-js')) return;
+            if (!document.querySelector('.prev-glide-js')) return;
             this.slider.mount();
         }, 500)
     }
@@ -16,4 +16,4 @@ const BlockSteps = class BlockSteps {
     }
 }
 
-export default BlockSteps;
+export default BlockPrev;
--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,8 @@ import Modals from '../blocks/modules/modals/modals.js';
 import BlockDistance from '../blocks/modules/block_distance/block_distance.js';
 import BlockReviews from '../blocks/modules/block_reviews/block_reviews.js';
 import BlockInfo from '../blocks/modules/block_info/block_info.js';
+import BlockSteps from '../blocks/modules/block_steps/block_steps.js';
+import BlockPrev from '../blocks/modules/block_prev/block_prev.js';
 
 window.app = new Vue({
     el: '#app',
@@ -17,6 +19,8 @@ window.app = new Vue({
         sizes: {
             tablet: 1024,
             mobile: 768,
+            width: window.innerWidth,
+            height: window.innerHeight,
             window: window.innerWidth
         },
         header: new Header({
@@ -26,6 +30,62 @@ window.app = new Vue({
             modalsSelector: "data-modal",
             modalsOpenerSelector: "data-modal-id",
             openedClass: "isOpened"
+        }),
+        BlockPrev: new BlockPrev({
+            sliderOptions: {
+                type: 'carousel',
+                perView: 4,
+                gap: 20,
+                startAt: 0,
+                breakpoints: {
+                    1340: {
+                        perView: 3,
+                        gap: 16,
+                    },
+                    1023: {
+                        perView: 2,
+                        gap: 16,
+                    },
+                    767: {
+                        perView: 1
+                    },
+                    480: {
+                        perView: 1,
+                        peek: {
+                            before: 16,
+                            after: 16
+                        }
+                    }
+                }
+            }
+        }),
+        BlockSteps: new BlockSteps({
+            sliderOptions: {
+                type: 'carousel',
+                perView: 4,
+                gap: 20,
+                startAt: 0,
+                breakpoints: {
+                    1340: {
+                        perView: 3,
+                        gap: 16,
+                    },
+                    1023: {
+                        perView: 2,
+                        gap: 16,
+                    },
+                    767: {
+                        perView: 1
+                    },
+                    480: {
+                        perView: 1,
+                        peek: {
+                            before: 16,
+                            after: 16
+                        }
+                    }
+                }
+            }
         }),
         BlockDistance: new BlockDistance({
             sliderOptions: {
@@ -110,12 +170,18 @@ window.app = new Vue({
         });
     },
     beforeMount() {
+        this.window = {
+            width: window.innerWidth,
+            height: window.innerHeight,
+        }
         this.isMounted = true;
         this.header.init();
         this.modals.init();
         this.BlockDistance.init();
         this.BlockInfo.init();
         this.BlockReviews.init();
+        this.BlockSteps.init();
+        this.BlockPrev.init();
     },
     computed: {
         isMobile: function () {
@@ -123,6 +189,17 @@ window.app = new Vue({
         },
         isTablet: function () {
             return this.sizes.window < this.sizes.tablet && this.sizes.window > this.sizes.mobile;
+        },
+        window: {
+            get() {
+                return {
+                    width: this.sizes.window.width,
+                    height: this.sizes.window.height
+                }
+            },
+            set(newValue) {
+                this.sizes.window = newValue;
+            }
         }
     },
 });
